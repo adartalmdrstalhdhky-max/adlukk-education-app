dfbdd055f336e9c5edcaf59672c0b6a2a5cf2eb6
adartalmdrstalhdhky-max <adartalmdrstalhdhky@gmail.com>
2025-11-30 22:51:40 +0300

    Update db.dart

--- app/lib/core/db.dart
+++ app/lib/core/db.dart
@@ -3,12 +3,18 @@ import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DbService {
-  static Future<Isar> init() async {
-    final dir = await getApplicationDocumentsDirectory();
-    final isar = await Isar.open(
-      schemas: [], // سنضع الـ schemas هنا لاحقًا (Book, Lesson, Progress)
+  static late Isar _isar;
+
+  static Future<void> init() async {
+    final dir = await getApplicationSupportDirectory();
+
+    // فتح قاعدة بيانات بدون أي Schema الآن
+    _isar = await Isar.open(
+      schemas: [],
       directory: dir.path,
+      inspector: false,
     );
-    return isar;
   }
+
+  static Isar get db => _isar;
 }
