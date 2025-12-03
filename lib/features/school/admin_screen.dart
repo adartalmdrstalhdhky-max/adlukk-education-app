2e9c4913fdd54cf70df1960a76ac07e85d5ba05b
adartalmdrstalhdhky-max <adartalmdrstalhdhky@gmail.com>
2025-12-03 21:24:01 +0300

    Create admin_screen.dart

---
+++ lib/features/school/admin_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AdminScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Admin Dashboard'),
+      ),
+      body: Center(
+        child: Text(
+          'Admin Panel',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
