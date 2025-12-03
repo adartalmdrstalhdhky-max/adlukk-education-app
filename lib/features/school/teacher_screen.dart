af465b14a487f377f0cdb659a378f889577d871e
adartalmdrstalhdhky-max <adartalmdrstalhdhky@gmail.com>
2025-12-03 21:25:10 +0300

    Create teacher_screen.dart

---
+++ lib/features/school/teacher_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TeacherScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Teacher Dashboard'),
+      ),
+      body: Center(
+        child: Text(
+          'Teacher Panel',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
