423eca589f1d909fa2b7b67a08576205cc4d74cc
adartalmdrstalhdhky-max <adartalmdrstalhdhky@gmail.com>
2025-12-04 21:12:41 +0300

    Create flutter_window.cpp

---
+++ windows/runner/flutter_window.cpp
@@ -0,0 +1,10 @@
+#include "flutter_window.h"
+#include <windows.h>
+
+FlutterWindow::FlutterWindow() {}
+
+FlutterWindow::~FlutterWindow() {}
+
+void FlutterWindow::OnCreate() {
+    // إعدادات نافذة Windows
+}
