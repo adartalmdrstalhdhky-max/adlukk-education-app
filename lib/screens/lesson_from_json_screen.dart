2de25fd3451e92a3b1697b3da544e6383b79f78b
عصام عبدالقوي احمد صالح <adartalmdrstalhdhky@gmail.com>
2025-12-19 18:45:46 +0300

    Update lesson_from_json_screen.dart

--- lib/screens/lesson_from_json_screen.dart
+++ lib/screens/lesson_from_json_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
-class LessonFromJsonScreen extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(title: Text('دروس النظام الرقمي')),
-body: Center(child: Text('يتم تحميل البيانات من ملف JSON')),
-);
+import 'dart:convert';
+
+class LessonFromJsonScreen extends StatefulWidget {
+  @override
+  _LessonFromJsonScreenState createState() => _LessonFromJsonScreenState();
 }
+
+class _LessonFromJsonScreenState extends State<LessonFromJsonScreen> {
+  List _lessons = [];
+
+  // دالة لجلب البيانات من ملف JSON محلي او رابط خارجي لادلك
+  Future<void> readJson() async {
+    // افترضنا وجود ملف دروس في المجلد assets
+    String response = '{"lessons": [{"id": 1, "title": "مقدمة في التقنية"}, {"id": 2, "title": "البرمجة للاجيال"}]}';
+    final data = await json.decode(response);
+    setState(() {
+      _lessons = data["lessons"];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    readJson();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('دروس منارة الاجيال')),
+      body: _lessons.isEmpty 
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _lessons.length,
+              itemBuilder: (ctx, index) => Card(
+                margin: EdgeInsets.all(10),
+                child: ListTile(
+                  title: Text(_lessons[index]["title"]),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+              ),
+            ),
+    );
+  }
 }
+
