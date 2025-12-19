fcacd4dcba28d8687f93c41ab13dabcb43e8af21
عصام عبدالقوي احمد صالح <adartalmdrstalhdhky@gmail.com>
2025-12-19 17:08:59 +0300

    Create root_shell.dart

---
+++ lib/screens/root_shell.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class RootShell extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+body: Center(child: Text('نظام تشغيل ادلك الاساسي')),
+);
+}
+}
